0153d95e5a7bf9170d3e2bd57dcc2a55547f0de0
Stepqueenvaishu <92020545+Stepqueenvaishu@users.noreply.github.com>
2022-02-17 16:38:13 +0530

    Add files via upload

---
+++ Arithmeticoperations.java
@@ -0,0 +1,22 @@
+package p1;
+
+public class Arithmeticoperations {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int s,d,p,q,r;
+		s=a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+s);
+		d=a-b;
+		System.out.println("The difference of "+a+" and "+b+" is "+s);
+		p=a*b;
+		System.out.println("The product of "+a+" and "+b+" is "+s);
+		q=a/b;
+		System.out.println("The quotient of "+a+" and "+b+" is "+s);
+		r=a%b;
+		System.out.println("The Remainder of "+a+" and "+b+" is "+s);
+
+	}
+
+}
